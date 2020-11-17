927175a390a198aec613033fa55c0847117ee932
Dennis <DennisLoska@users.noreply.github.com>
2020-11-17 16:02:37 +0100

    🥅 add: error handling & request-body validation

--- routes/routes.js
+++ routes/routes.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 const CallModel = require('../models/call');
 
 module.exports = (app) => {
@@ -8,34 +9,87 @@ module.exports = (app) => {
 
   // create call
   app.post('/create_call', async (req, res) => {
-    const newCall = new CallModel(req.body);
-    const { _id } = newCall;
-    try {
-      await newCall.save();
-    } catch (error) {
-      return res.status(500).send('Failed to create new call.');
+    // always validate request body/parameters
+    const allowedParameters = ['transcription', 'phone_number', 'callback', 'country'];
+    const sanitizedBody = {};
+
+    // sanitize request body
+    allowedParameters.forEach((allowedProp) => {
+      if (req.body.hasOwnProperty(allowedProp)) {
+        sanitizedBody[allowedProp] = req.body[allowedProp];
+      }
+    });
+
+    //validate parameters (validate all important ones)
+    if (typeof sanitizedBody.transcription === 'string') {
+      const newCall = new CallModel(sanitizedBody);
+      const { _id } = newCall;
+
+      // always catch potential promise errors
+      try {
+        await newCall.save();
+      } catch (error) {
+        return res.status(500).send('Failed to create new call.');
+      }
+
+      const call = await CallModel.findById(_id);
+      console.log(`Created a call: ${JSON.stringify(call)}`);
+      return res.status(200).json(JSON.stringify(call));
     }
-    const call = await CallModel.findById(_id);
-    console.log(`Created a call: ${JSON.stringify(call)}`);
-    return res.status(200).json(JSON.stringify(call));
+    return res.status(400).send('Bad request parameter.');
   });
 
   // get calls
-  app.get('/calls', async (req, res) => {
-    // https://mongoosejs.com/docs/promises.html
-    const calls = await CallModel.find();
+  app.get('/get_calls', async (req, res) => {
+    let calls;
+
+    // always catch potential promise errors
+    try {
+      calls = await CallModel.find();
+    } catch (error) {
+      return res.status(500).send('Failed to get calls.');
+    }
+
     console.log('Read all calls.');
-    res.status(200).json(JSON.stringify(calls));
+    return res.status(200).json(JSON.stringify(calls));
   });
 
-  // update
-  // todo
+  // update call
+  app.post('/update_call', async (req, res) => {
+    const { id, message } = req.body;
+
+    // always validate request body/parameters
+    if (typeof message === 'string' && typeof id === 'string') {
+      const query = { _id: id };
+      const updatedValue = { $set: { transcription: message } };
+
+      // always catch potential promise errors
+      try {
+        await CallModel.updateOne(query, updatedValue);
+      } catch (error) {
+        return res.status(500).send('Failed to create new call.');
+      }
+
+      const call = await CallModel.findById(id);
+      console.log(`Updated call: ${JSON.stringify(call)}`);
+      return res.status(200).json(JSON.stringify(call));
+    }
+    return res.status(400).send('Bad request parameter.');
+  });
 
   // get calls
   app.get('/delete_calls', async (req, res) => {
-    await CallModel.deleteMany();
-    const calls = await CallModel.find();
+    let calls;
+
+    // always catch potential promise errors
+    try {
+      await CallModel.deleteMany();
+      calls = await CallModel.find();
+    } catch (error) {
+      return res.status(500).send('Failed to create new call.');
+    }
+
     console.log('Deleted all calls.');
-    res.status(200).json(JSON.stringify(calls));
+    return res.status(200).json(JSON.stringify(calls));
   });
 };
